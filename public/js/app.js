763e3d19eb1702bef0c1434a1707b671bc04c044
YOUR FULL NAME <farishta.haider@gmail.com>
2016-04-28 13:00:13 -0400

    Photo and Boombox working properly w angular

--- public/js/app.js
+++ public/js/app.js
@@ -16,18 +16,18 @@
   //   "$resource",
   //   BeachhouseFactory
   // ])
-  .factory("Mixologist", [
-    "$resource",
-    MixologistFactory
-  ])
+  // .factory("Mixologist", [
+  //   "$resource",
+  //   MixologistFactory
+  // ])
   // .factory("Boombox", [
   //   "$resource",
   //   BoomboxFactory
   // ])
-  // .factory("Photobooth", [
-  //   "$resource",
-  //   PhotoboothFactory
-  // ])
+  .factory("Photobooth", [
+    "$resource",
+    PhotoboothFactory
+  ])
   // .controller("BHIndexCtrl", [
   //   "BeachhouseFactory",
   //   BHIndexCtrlFunction
@@ -37,15 +37,15 @@
   //   "$stateParams",
   //   BHShowCtrlFunction
   // ])
-  .controller("MixIndexCtrl", [
-    "MixologistFactory",
-    MixIndexCtrlFunction
-  ])
-  .controller("MixShowCtrl", [
-    "MixologistFactory",
-    "$stateParams",
-    "$window",
-    MixShowCtrlFunction
+  // .controller("MixIndexCtrl", [
+  //   "MixologistFactory",
+  //   MixIndexCtrlFunction
+  // ])
+  // .controller("MixShowCtrl", [
+  //   "MixologistFactory",
+  //   "$stateParams",
+  //   "$window",
+  //   MixShowCtrlFunction
   // ])
   // .controller("BoomIndexCtrl", [
   //   "BoomboxFactory",
@@ -57,15 +57,15 @@
   //   "$window",
   //   BoomShowCtrlFunction
   // ])
-  // .controller("PhotoIndexCtrl", [
-  //   "PhotoboothFactory",
-  //   PhotoIndexCtrlFunction
-  // ])
-  // .controller("PhotoShowCtrl", [
-  //   "PhotoboothFactory",
-  //   "$stateParams",
-  //   "$window",
-  //   PhotoShowCtrlFunction
+  .controller("PhotoIndexCtrl", [
+    "PhotoboothFactory",
+    PhotoIndexCtrlFunction
+  ])
+  .controller("PhotoShowCtrl", [
+    "PhotoboothFactory",
+    "$stateParams",
+    "$window",
+    PhotoShowCtrlFunction
   // ])
   // .controller("ContestIndexCtrl", [
   //   "ContestFactory",
@@ -113,17 +113,17 @@
     //   controller: "BHShowCtrl",
     //   controllerAs: "BHShowVM"
     // })
-    .state("MixIndex", {
-      url: "/mixologist",
-      templateUrl: "assets/html/mixologist-index.html",
-      controller: "MixIndexCtrl",
-      controllerAs: "MixIndexVM"
-    })
-    .state("MixShow",{
-      url: "/mixologist/:drink_name",
-      templateUrl: "assets/html/mixologist-show.html",
-      controller: "MixShowCtrl",
-      controllerAs: "MixShowVM"
+    // .state("MixIndex", {
+    //   url: "/mixologist",
+    //   templateUrl: "assets/html/mixologist-index.html",
+    //   controller: "MixIndexCtrl",
+    //   controllerAs: "MixIndexVM"
+    // })
+    // .state("MixShow",{
+    //   url: "/mixologist/:drink_name",
+    //   templateUrl: "assets/html/mixologist-show.html",
+    //   controller: "MixShowCtrl",
+    //   controllerAs: "MixShowVM"
     // })
     // .state("BoomIndex",{
     //   url: "/boombox",
@@ -137,17 +137,17 @@
     //   controller: "BoomShowCtrl",
     //   controllerAs:"BoomShowVM"
     // })
-    // .state("PhotoIndex",{
-    //   url: "/photobooth",
-    //   templateUrl: "assets/html/photobooth-index.html",
-    //   controller: "PhotoIndexCtrl",
-    //   controllerAs:"PhotoIndexVM"
-    // })
-    // .state("PhotoShow",{
-    //   url: "/photobooth/:photo_name",
-    //   templateUrl: "assets/html/photobooth-show.html",
-    //   controller: "PhotoShowCtrl",
-    //   controllerAs:"PhotoShowVM"
+    .state("PhotoIndex",{
+      url: "/photobooth",
+      templateUrl: "assets/html/photobooth-index.html",
+      controller: "PhotoIndexCtrl",
+      controllerAs:"PhotoIndexVM"
+    })
+    .state("PhotoShow",{
+      url: "/photobooth/:photo_name",
+      templateUrl: "assets/html/photobooth-show.html",
+      controller: "PhotoShowCtrl",
+      controllerAs:"PhotoShowVM"
     // })
     // .state("ContestIndex",{
     //   url: "/Contest",
@@ -193,106 +193,239 @@
   //   vm.beachhouses = Beachhouse.all;
   // };
 
-  function MixologistFactory($resource){
-    var Mixologist = $resource("/api/mixologist/:drink_name",
-    {}, {
-      update: {method: "PUT"},
-      like: {
-        method: "POST",
-        url:"/api/mixologist/:drink_name/like",
-        params: {
-          name: "@name"
-        }
-      }
-    });
-    Mixologist.all = Mixologist.query();
-    Mixologist.find = function(property, value, callback){
-      Mixologist.$promise.then(function(){
-        Mixologist.forEach(function(mixologist){
-          if(mixologist[property]== value) callback(mixologist);
-        });
-      });
-    }
-    return Mixologist;
-  }
-  function MixIndexCtrlFunction(Mixologist){
-    var vm = this;
-    vm.mixologists = Mixologist.all;
-  }
-  function MixShowCtrlFunction(Mixologist, $stateParams,  $window){
-    var vm = this;
-    Mixologist.find("drink_name", $stateParams.drink_name, function(mixologist){
-      vm.mixologist = mixologist;
-    });
-    vm.update = function(){
-      Mixologist.update({name: vm.mixologist.drink_name}, {mixologist: vm.mixologist}, function(){
-        console.log("Dizun!");
-      });
-    }
-    vm.delete = function(){
-      Mixologist.remove({ name: vm.mixologist.drink_name}, function(){
-      $window.location.replace("/");
-      });
-    }
-    vm.addDrink = function(){
-      if(vm.mixologist.drinks.includes(vm.newDrink)){
-        console.log("This is a duplicate");
-      }else{
-        vm.mixologist.drinks.push(vm.newDrink);
-        vm.newDrink = "";
-        vm.update();
-      }
-    }
-    vm.removeDrink = function($index){
-      vm.mixologist.drinks.splice($index, 1);
-      vm.update();
-    }
-    vm.like = function(){
-      Mixologist.like(vm.mixologist, function(response){
-        vm.mixologist.likedBy = response.likedBy;
-      });
-    }
-  }
-
-
-
-
-
-  //
-  // function BoomboxFactory($resource){
-  //   var Boombox = $resource("/boombox/playlist_name",
+  // function MixologistFactory($resource){
+  //   var Mixologist = $resource("/api/mixologist/:drink_name",
   //   {}, {
-  //     update: {method: "PUT"}
+  //     update: {method: "PUT"},
+  //     like: {
+  //       method: "POST",
+  //       url:"/api/mixologist/:drink_name/like",
+  //       params: {
+  //         name: "@name"
+  //       }
+  //     }
+  //   });
+  //   Mixologist.all = Mixologist.query();
+  //   Mixologist.find = function(property, value, callback){
+  //     Mixologist.$promise.then(function(){
+  //       Mixologist.forEach(function(mixologist){
+  //         if(mixologist[property]== value) callback(mixologist);
+  //       });
+  //     });
+  //   }
+  //   return Mixologist;
+  // }
+  // function MixIndexCtrlFunction(Mixologist){
+  //   var vm = this;
+  //   vm.mixologists = Mixologist.all;
+  // }
+  // function MixShowCtrlFunction(Mixologist, $stateParams,  $window){
+  //   var vm = this;
+  //   Mixologist.find("drink_name", $stateParams.drink_name, function(mixologist){
+  //     vm.mixologist = mixologist;
+  //   });
+  //   vm.update = function(){
+  //     Mixologist.update({name: vm.mixologist.drink_name}, {mixologist: vm.mixologist}, function(){
+  //       console.log("Dizun!");
+  //     });
+  //   }
+  //   vm.delete = function(){
+  //     Mixologist.remove({ name: vm.mixologist.drink_name}, function(){
+  //     $window.location.replace("/");
+  //     });
+  //   }
+  //   vm.addDrink = function(){
+  //     if(vm.mixologist.drinks.includes(vm.newDrink)){
+  //       console.log("This is a duplicate");
+  //     }else{
+  //       vm.mixologist.drinks.push(vm.newDrink);
+  //       vm.newDrink = "";
+  //       vm.update();
+  //     }
+  //   }
+  //   vm.removeDrink = function($index){
+  //     vm.mixologist.drinks.splice($index, 1);
+  //     vm.update();
+  //   }
+  //   vm.like = function(){
+  //     Mixologist.like(vm.mixologist, function(response){
+  //       vm.mixologist.likedBy = response.likedBy;
+  //     });
+  //   }
+  // }
+  //
+  //
+  // Mixologist.all = Mixologist.query();
+  // Mixologist.find = function(property, value, callback){
+  //   Mixologist.$promise.then(function(){
+  //     Mixologist.forEach(function(mixologist){
+  //       if(mixologist[property]== value) callback(mixologist);
+  //     });
+  //   });
+  // }
+  // return Mixologist;
+  // }
+  // function MixIndexCtrlFunction(Mixologist){
+  // var vm = this;
+  // vm.mixologists = Mixologist.all;
+  // }
+  // function MixShowCtrlFunction(Mixologist, $stateParams,  $window){
+  // var vm = this;
+  // Mixologist.find("drink_name", $stateParams.drink_name, function(mixologist){
+  //   vm.mixologist = mixologist;
+  // });
+  // vm.update = function(){
+  //   Mixologist.update({name: vm.mixologist.drink_name}, {mixologist: vm.mixologist}, function(){
+  //     console.log("Dizun!");
+  //   });
+  // }
+  // vm.delete = function(){
+  //   Mixologist.remove({ name: vm.mixologist.drink_name}, function(){
+  //   $window.location.replace("/");
+  //   });
+  // }
+  // vm.addDrink = function(){
+  //   if(vm.mixologist.drinks.includes(vm.newDrink)){
+  //     console.log("This is a duplicate");
+  //   }else{
+  //     vm.mixologist.drinks.push(vm.newDrink);
+  //     vm.newDrink = "";
+  //     vm.update();
+  //   }
+  // }
+  // vm.removeDrink = function($index){
+  //   vm.mixologist.drinks.splice($index, 1);
+  //   vm.update();
+  // }
+  // vm.like = function(){
+  //   Mixologist.like(vm.mixologist, function(response){
+  //     vm.mixologist.likedBy = response.likedBy;
+  //   });
+  // }
+  // }
+
+  // function BoomboxFactory($resource){
+  //   var Boombox = $resource("/api/boombox/:playlist_name",
+  //   {}, {
+  //     update: {method: "PUT"},
+  //     like: {
+  //       method: "POST",
+  //       url:"/api/boombox/:playlist_name/like",
+  //       params: {
+  //         name: "@name"
+  //       }
+  //     }
   //   });
   //   Boombox.all = Boombox.query();
+  //   Boombox.find = function(property, value, callback){
+  //     Boombox.$promise.then(function(){
+  //       Boombox.forEach(function(boombox){
+  //         if(boombox[property]== value) callback(boombox);
+  //       });
+  //     });
+  //   }
   //   return Boombox;
   // }
   // function BoomIndexCtrlFunction(Boombox){
   //   var vm = this;
   //   vm.boomboxes = Boombox.all;
   // }
-  // function BoomShowCtrlFunction(Boombox){
+  // function BoomShowCtrlFunction(Boombox, $stateParams,  $window){
   //   var vm = this;
-  //   vm.boomboxes = Boombox.all;
-  // };
-  //
-  // function PhotoboothFactory($resource){
-  //   var Photobooth = $resource("/photobooth/photo_name",
-  //   {}, {
-  //     update: {method: "PUT"}
+  //   Boombox.find("playlist_name", $stateParams.playlist_name, function(boombox){
+  //     vm.boombox = boombox;
   //   });
-  //   Photobooth.all = Photobooth.query();
-  //   return Photobooth;
+  //   vm.update = function(){
+  //     Boombox.update({name: vm.boombox.playlist_name}, {boombox: vm.boombox}, function(){
+  //       console.log("Dizun!");
+  //     });
+  //   }
+  //   vm.delete = function(){
+  //     Boombox.remove({ name: vm.boombox.playlist_name}, function(){
+  //     $window.location.replace("/");
+  //     });
+  //   }
+  //   vm.addMusic = function(){
+  //     if(vm.boombox.musics.includes(vm.newMusic)){
+  //       console.log("This is a duplicate");
+  //     }else{
+  //       vm.boombox.musics.push(vm.newMusic);
+  //       vm.newMusic = "";
+  //       vm.update();
+  //     }
+  //   }
+  //   vm.removeMusic = function($index){
+  //     vm.boombox.musics.splice($index, 1);
+  //     vm.update();
+  //   }
+  //   vm.like = function(){
+  //     Boombox.like(vm.boombox, function(response){
+  //       vm.boombox.likedBy = response.likedBy;
+  //     });
+  //   }
   // }
-  // function PhotoIndexCtrlFunction(Photobooth){
-  //   var vm = this;
-  //   vm.photobooths = Photobooth.all;
-  // }
-  // function PhotoShowCtrlFunction(Photobooth){
-  //   var vm = this;
-  //   vm.photobooths = Photobooth.all;
-  // };
-  //
+
+  function PhotoboothFactory($resource){
+    var Photobooth = $resource("/api/photobooth/:photo_name",
+    {}, {
+      update: {method: "PUT"},
+      like: {
+        method: "POST",
+        url:"/api/photobooth/:photo_name/like",
+        params: {
+          name: "@name"
+        }
+      }
+    });
+    Photobooth.all = Photobooth.query();
+    Photobooth.find = function(property, value, callback){
+      Photobooth.$promise.then(function(){
+        Photobooth.forEach(function(photobooth){
+          if(photobooth[property]== value) callback(photobooth);
+        });
+      });
+    }
+    return Photobooth;
+  }
+  function PhotoIndexCtrlFunction(Photobooth){
+    var vm = this;
+    vm.photobooths = Photobooth.all;
+  }
+  function PhotoShowCtrlFunction(Photobooth, $stateParams,  $window){
+    var vm = this;
+    Photobooth.find("photo_name", $stateParams.photo_name, function(photobooth){
+      vm.photobooth = photobooth;
+    });
+    vm.update = function(){
+      Photobooth.update({name: vm.photobooth.photo_name}, {photobooth: vm.photobooth}, function(){
+        console.log("Dizun!");
+      });
+    }
+    vm.delete = function(){
+      Photobooth.remove({ name: vm.photobooth.photo_name}, function(){
+      $window.location.replace("/");
+      });
+    }
+    vm.addPhoto = function(){
+      if(vm.photobooth.musics.includes(vm.newPhoto)){
+        console.log("This is a duplicate");
+      }else{
+        vm.photobooth.musics.push(vm.newPhoto);
+        vm.newPhoto = "";
+        vm.update();
+      }
+    }
+    vm.removePhoto = function($index){
+      vm.photobooth.musics.splice($index, 1);
+      vm.update();
+    }
+    vm.like = function(){
+      Photobooth.like(vm.photobooth, function(response){
+        vm.photobooth.likedBy = response.likedBy;
+      });
+    }
+  }
+
   // function ContestFactory($resource){
   //   var Contest = $resource("/contest/entry",
   //   {}, {
