ec63aa888f6a254741e9e344286b2a6e8045d057
YOUR FULL NAME <farishta.haider@gmail.com>
2016-07-16 11:25:35 -0400

    changed layout script links

--- db/seed.js
+++ db/seed.js
@@ -6,9 +6,7 @@ var seedData = require("./contests");
 // var calendarData = require("./calendars")
 
 // var Mixologist = mongoose.model("Mixologist");
-// var Boombox = mongoose.model("Boombox");
 // var Photobooth = mongoose.model("Photobooth");
-// var Calendar = mongoose.model("Calendar");
 var Contest = mongoose.model("Contest");
 
 // Mixologist.remove({}).then(function(){
@@ -18,23 +16,12 @@ var Contest = mongoose.model("Contest");
 //   });
 // });
 
-// Boombox.remove({}).then(function(){
-//   Boombox.collection.insert(musicData).then(function(){
-//     process.exit();
-//   });
-// });
-
 // Photobooth.remove({}).then(function(){
 //   Photobooth.collection.insert(photoData).then(function(){
 //     process.exit();
 //   });
 // });
-//
-// Calendar.remove({}).then(function(){
-//   Calendar.collection.insert(calendarData).then(function(){
-//     process.exit();
-//   });
-// });
+
 
 Contest.remove({}).then(function(){
   Contest.collection.insert(seedData).then(function(){
